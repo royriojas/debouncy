cf9bc135a674c9a77ca48a72fe2bc21a67459c37
royriojas <roy@kno.com>
2015-07-01 03:17:18 -0700

    FEAT: debouncy first working version

---
+++ index.js
@@ -0,0 +1,57 @@
+
+var now = function () {
+  return Date.now();
+};
+
+/**
+ * returns a new function than will be called after "ms" number of milliseconds
+ * after the last call to it
+ *
+ * This is useful to execute a function that might occur too often
+ *
+ * @method debounce
+ * @static
+ * @param f {Function} the function to debounce
+ * @param ms {Number} the number of milliseconds to wait. If any other call
+ * is made before that threshold the waiting will be restarted
+ * @param [ctx=undefined] {Object} the context on which this function will be executed
+ * (the 'this' object inside the function wil be set to context)
+ * @param [immediate=undefined] {Boolean} if the function should be executed in the leading edge or the trailing edge
+ * ```
+ */
+module.exports = function debounce( f, ms, ctx, immediate ) {
+  var ts, fn, timeout;
+
+  fn = function () {
+    ctx = ctx || this;
+    var args = arguments;
+    ts = now();
+
+    var later = function () {
+      var diff = now() - ts;
+
+      if ( diff < ms ) {
+        timeout = setTimeout( later, ms - diff );
+        return;
+      }
+      timeout = null;
+
+      if ( !immediate ) {
+        f.apply( ctx, args );
+      }
+    };
+
+    if ( !timeout ) {
+      if ( immediate ) {
+        f.apply( ctx, args );
+      }
+      timeout = setTimeout( later, ms );
+    }
+  };
+
+  fn.cancel = function () {
+    clearTimeout( timeout );
+  };
+
+  return fn;
+};
---
+++ specs/debouncy.js
@@ -0,0 +1,100 @@
+describe( 'debouncy', function () {
+  beforeEach( function () {
+    var me = this;
+    me.clock = me.sandbox.useFakeTimers();
+  } );
+
+  afterEach( function () {
+    var me = this;
+    me.clock.restore();
+  } );
+
+  it( 'should execute a function as soon as the time between to the last call to it is equal to the provided delay', function () {
+    var me = this;
+
+    var spyFn = this.sandbox.spy();
+
+    var debouncy = require( '../index' );
+
+    var delayedFn = debouncy( spyFn, 100 );
+
+    delayedFn();
+    expect( spyFn.callCount ).to.equal( 0 );
+
+    me.clock.tick( 50 );
+
+    expect( spyFn.callCount ).to.equal( 0 );
+
+    delayedFn();
+    me.clock.tick( 50 );
+
+    expect( spyFn.callCount ).to.equal( 0 );
+
+    delayedFn();
+    me.clock.tick( 100 );
+
+    expect( spyFn.callCount ).to.equal( 1 );
+
+  } );
+
+  it( 'should not call the function if cancel is called on the debounced function', function () {
+    var me = this;
+
+    var spyFn = this.sandbox.spy();
+
+    var debouncy = require( '../index' );
+
+    var delayedFn = debouncy( spyFn, 100 );
+
+    delayedFn();
+    expect( spyFn.callCount ).to.equal( 0 );
+
+    me.clock.tick( 50 );
+
+    expect( spyFn.callCount ).to.equal( 0 );
+
+    delayedFn();
+
+    me.clock.tick( 50 );
+
+    expect( spyFn.callCount ).to.equal( 0 );
+
+    delayedFn();
+    me.clock.tick( 50 );
+    delayedFn.cancel();
+
+    me.clock.tick( 100 );
+
+    expect( spyFn.callCount ).to.equal( 0 );
+
+  } );
+
+
+  it( 'should execute a function on the leading edge of the sequence of calls', function () {
+    var me = this;
+
+    var spyFn = this.sandbox.spy();
+
+
+    var debouncy = require( '../index' );
+
+    var delayedFn = debouncy( spyFn, 100, null, true );
+
+    delayedFn();
+    expect( spyFn.callCount ).to.equal( 1 );
+
+    me.clock.tick( 50 );
+
+    delayedFn();
+    me.clock.tick( 50 );
+
+    expect( spyFn.callCount ).to.equal( 1 );
+    me.clock.tick( 50 );
+
+    delayedFn();
+    me.clock.tick( 100 );
+
+    expect( spyFn.callCount ).to.equal( 2 );
+
+  } );
+} );
